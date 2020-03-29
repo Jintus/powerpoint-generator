5f06274c83dc42bd32182f0e31e22ed35720ee4d
Justin GALLY <justin.gally@capgemini.com>
2020-03-29 20:40:22 +0200

    Add images

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const Excel = require('exceljs');
 // const assetsFolder = path.join(process.cwd(), 'assets');
 // Uncomment when packaging the app (& comment previous line)
 const assetsFolder = path.join(path.dirname(process.execPath), 'assets');
+const imagesFolder = path.join(assetsFolder, "images");
 const configFile = path.join(assetsFolder, 'generator.config.json');
 const GeneratorConfig = JSON.parse(FileSystem.readFileSync(configFile));
 const KPIExcelConfig = GeneratorConfig.kpiExcelFileConfig;
@@ -36,14 +37,34 @@ async function loadKpiFromExcelFile(filename) {
     }));
 }
 
+function addEvolutionImageToSlide(slide, value, positionX, positionY, sizeX, sizeY) {
+    let imageFilename = GeneratorConfig.images.stable;
+    if (value < 0)
+        imageFilename = GeneratorConfig.images.descending;
+    else if (value > 0)
+        imageFilename = GeneratorConfig.images.ascending;
+    console.log(`Adding image ${imageFilename} to ${slide.name}`);
+    slide.addImage(image => {
+        image
+            .file(path.join(imagesFolder, imageFilename))
+            .x(positionX)
+            .y(positionY)
+            .cx(sizeX)
+            .cy(sizeY)
+    });
+}
+
 async function editPowerPoint(kpis, filename, outputFilename) {
     console.log(`Generating PowerPoint from ${filename}`);
     const pptx = new PPTX.Composer();
     await pptx.load(filename);
     await pptx.compose(async pres => {
-        Object.values(pres.powerPointFactory.slides).forEach(slide => {
+        Object.values(pres.powerPointFactory.slides).forEach((slide, index) => {
             let stringifiedSlideContent = JSON.stringify(slide.content['p:sld']);
             Object.entries(kpis).forEach(([kpi, value]) => {
+                if (kpi.substr(0, 3) === "img") {
+                    return;
+                }
                 if (kpi.substr(0, 2) === "tv") {
                     kpi = kpi.substr(2);
                     if (Number.isFinite(value)) {
@@ -57,6 +78,12 @@ async function editPowerPoint(kpis, filename, outputFilename) {
                 stringifiedSlideContent = stringifiedSlideContent.replace(new RegExp(kpi, 'g'), value);
             });
             slide.content['p:sld'] = JSON.parse(stringifiedSlideContent);
+            if (index === 3) {
+                addEvolutionImageToSlide(slide, kpis["img_1"], 150, 210, 30, 30);
+                addEvolutionImageToSlide(slide, kpis["img_2"], 800, 210, 30, 30);
+                addEvolutionImageToSlide(slide, kpis["img_3"], 150, 330, 30, 30);
+                addEvolutionImageToSlide(slide, kpis["img_4"], 800, 330, 30, 30);
+            }
         });
     });
     await pptx.save(outputFilename);
